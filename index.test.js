3b55128474116bcecccaadf608822f19bf01b009
sae-github <miyasae1126@gmail.com>
2021-08-05 21:49:05 +0900

    fix: index.test.js
    Add a response to Assignment 3
    
    Remove the if statement

--- index.test.js
+++ index.test.js
@@ -5,41 +5,47 @@ test('adds undefined + 2 to equal NaN', () => {
 
 
 // lesson 1
+import { addString } from "./index";
 describe("addString", () => {
-  it('addString a + b equal  "ab" ', () => {
-    if (typeof a === "string" || typeof b === "string")
-      expect(addString(a, b)).toBe('ab');
+  it('addString a + b equal "ab" ', () => {
+    expect(addString('a', 'b')).toBe('ab');
   });
-  it('addString a +  b equal  a1', () => {
-    if (typeof a === "string" && b === 1) {
-      expect(addString(a, b)).toBe('a1');
-    }
+  it('addString a + b equal  a1', () => {
+    expect(addString('a', 1)).toBe('a1');
   });
   it('addString a + 1 b equal -1', () => {
-    if (typeof a === 1 && b === 1) {
-      expect(addString(a, b)).toBe(-1);
-    }
+    expect(addString(1, 1)).toBe(-1);
   });
 })
 
 // lesson2
 import { toNumber } from "./index";
-  describe("toNumber",() => {
-    it('toNumber 0.1 equal 0',() => {
-      expect(toNumber(0.1)).toBe(0);
-    });
-      it('toNumber a equal NaN',() => {
+describe("toNumber", () => {
+  it('toNumber 0.1 equal 0', () => {
+    expect(toNumber(0.1)).toBe(0);
+  });
+  it('toNumber a equal NaN', () => {
     expect(toNumber('a')).toBe(NaN);
-    });
-    it('toNumber 1000a equal 1000',() => {
+  });
+  it('toNumber 1000a equal 1000', () => {
     expect(toNumber('1000a')).toBe(1000);
   });
-  });
-
-
-  // lesson3
-import { filterOrange } from "./index"; 
-const fruit = ["orange", "apple", "peach"];
-test('filterOrange equal orange', () => {
-  expect(filterOrange(fruit)).toContain('orange');
 });
+
+
+// lesson3
+import { filterOrange } from "./index";
+const fruit = ["orange", "apple", "peach"];
+describe("filterOrange", () => {
+  it('filterOrange equal orange', () => {
+    expect(filterOrange(fruit)).toContain('orange');
+  });
+  it('filterOrange no orange', () => {
+    expect(() => {
+      filterOrange(false);
+    }).toThrow();
+  });
+});
+
+
+
